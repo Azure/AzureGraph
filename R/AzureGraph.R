f84ad3b3d4ad46e6a4e24cb599a598570241ea84
Hong Ooi <hongooi@microsoft.com>
2019-03-24 02:33:55 +1100

    don't create token dir

--- R/AzureGraph.R
+++ R/AzureGraph.R
@@ -15,20 +15,3 @@ utils::globalVariables(c("self", "private"))
 # default authentication app ID: leverage the az CLI
 .az_cli_app_id <- "04b07795-8ddb-461a-bbee-02f9e1bf7b46"
 
-
-# create a directory for saving creds -- ask first, to satisfy CRAN requirements
-make_AzureR_dir <- function()
-{
-    AzureR_dir <- AzureR_dir()
-    if(!dir.exists(AzureR_dir) && interactive())
-    {
-        yn <- readline(paste0(
-                "AzureGraph can cache Microsoft Graph logins in the directory:\n\n",
-                AzureR_dir, "\n\n",
-                "This saves you having to re-authenticate with Azure in future sessions. Create this directory? (Y/n) "))
-        if(tolower(substr(yn, 1, 1)) == "n")
-            return(invisible(NULL))
-
-        dir.create(AzureR_dir, recursive=TRUE)
-    }
-}
