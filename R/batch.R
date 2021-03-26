542dac30c69ecc59f3cd25109f87a2d3dfe110ff
Hong Ooi <hongooi73@gmail.com>
2021-03-27 05:15:03 +1100

    document

--- R/batch.R
+++ R/batch.R
@@ -1,3 +1,43 @@
+#' Microsoft Graph request
+#'
+#' Class representing a request to the Microsoft Graph API. Currently this is used only in building a batch call.
+#'
+#' @docType class
+#' @section Methods:
+#' - `new(...)`: Initialize a new request object with the given parameters. See 'Details' below.
+#' - `batchify()`: Generate a list object suitable for incorporating into a call to the batch endpoint.
+#' @section Details:
+#' The `initialize()` method takes the following arguments, representing the components of a HTTPS request:
+#' - `op`: The path of the HTTPS URL, eg `/me/drives`.
+#' - `body`: The body of the HTTPS request, if it is a PUT, POST or PATCH.
+#' - `options`: A list containing the query parameters for the URL.
+#' - `headers`: Any optional HTTP headers for the request.
+#' - `encode`: If a request body is present, how it should be encoded when sending it to the endpoint. The default is `json`, meaning it will be sent as JSON text; an alternative is `raw`, for binary data.
+#' - `http_verb`: One of "GET" (the default), "DELETE", "PUT", "POST", "HEAD", or "PATCH".
+#'
+#' This class is currently used only for building batch calls. Future versions of AzureGraph may be refactored to use it in general API calls as well.
+#' @seealso
+#' [call_batch_endpoint]
+#'
+#' [Microsoft Graph overview](https://docs.microsoft.com/en-us/graph/overview),
+#' [Batch endpoint documentation](https://docs.microsoft.com/en-us/graph/json-batching)
+#'
+#' @examples
+#' graph_request$new("me")
+#'
+#' # a new email message in Outlook
+#' graph_request$new("me/messages",
+#'     body=list(
+#'         body=list(
+#'             content="Hello from R",
+#'             content_type="text"
+#'         ),
+#'         subject="Hello",
+#'         toRecipients="bob@example.com"
+#'     ),
+#'     http_verb="POST"
+#' )
+#' @format An R6 object of class `graph_request`.
 #' @export
 graph_request <- R6::R6Class("graph_request",
 
@@ -51,6 +91,47 @@ public=list(
 ))
 
 
+#' Call the Graph API batch endpoint
+#'
+#' @param token An Azure OAuth token, of class [AzureToken].
+#' @param requests A list of [graph_request] objects, representing individual requests to the Graph API.
+#' @param depends_on An optional named list indicating which requests have dependencies. See the examples below.
+#' @param api_version The API version to use, which will form part of the URL sent to the host.
+#'
+#' @details
+#' Use this function to combine multiple requests into a single HTTPS call. This can save significant network latency.
+#'
+#' @seealso
+#' [graph_request], [call_graph_endpoint]
+#'
+#' [Microsoft Graph overview](https://docs.microsoft.com/en-us/graph/overview),
+#' [Batch endpoint documentation](https://docs.microsoft.com/en-us/graph/json-batching)
+#'
+#' @examples
+#' \dontrun{
+#'
+#' req1 <- graph_request$new("me")
+#'
+#' # a new email message in Outlook
+#' req2 <- graph_request$new("me/messages",
+#'     body=list(
+#'         body=list(
+#'             content="Hello from R",
+#'             content_type="text"
+#'         ),
+#'         subject="Hello",
+#'         toRecipients="bob@example.com"
+#'     ),
+#'     http_verb="POST"
+#' )
+#'
+#' # messages in drafts
+#' req3 <- graph_request$new("me/mailFolders/drafts/messages")
+#'
+#' # depends_on signifies that request 3 depends on request 2
+#' call_batch_endpoint(token, list(req1, req2, req3), depends_on=list("3"=2))
+#'
+#' }
 #' @export
 call_batch_endpoint <- function(token, requests=list(), depends_on=list(),
                                 api_version=getOption("azure_graph_api_version"))
